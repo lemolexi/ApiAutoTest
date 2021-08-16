6317028df4fb734d1ebbeb8a68d4ee103eb502ec
wanglin <wanglin>
2021-08-16 10:28:39 +0800

    新增

---
+++ Auto/Chartep1/src/main/java/demo.java
@@ -0,0 +1,15 @@
+/**
+ * FileName: demo
+ * Author:   wanglin
+ * Date:     2021/8/16 10:25
+ * Description: ${DESCRIPTION}
+ * version: IT2021${Iteration}
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public class demo {
+    public static void main(String[] args){
+        System.out.println("hello world !!");
+    }
+}
